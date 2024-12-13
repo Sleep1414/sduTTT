26bcf53b56191d46957d8c77f55d53cd599b6c76
Tom <tom@pixelprinzen.de>
2024-12-13 19:02:04 +0100

    added switch case: sout where the next move must be

--- src/src/UI.java
+++ src/src/UI.java
@@ -24,6 +24,8 @@ public class UI {
     //false == player2
     boolean whichplayerturn = true;
     JLabel currentPlayerL;
+    Pos lastmove;
+    Pos nextmove;
 
 
     private void createPlayWindow() {
@@ -121,6 +123,9 @@ public class UI {
         String actionCommand = position + "-" + pos;
         cellMap.put(actionCommand, innerCell);
 
+
+        //funktion wo geschaut wird wo man hinplacen darf
+
         innerCell.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -130,17 +135,23 @@ public class UI {
                 }
 
                 if (!whichplayerturn) {
+                    System.out.println("Spieler 1 at: " + position+"-"+pos);
+                    nextplayerneedto(pos, position);
                     // Spieler 1
                     innerCell.setText("<html><span style=\"color: red; font-size: 20px;\">x</span></html>");
                     //System.out.println("Spieler 1 ist am Zug");
                     currentPlayerL.setText("Am Zug: Spieler 2");
                     whichplayerturn = true;
+
                 } else {
+                    System.out.println("Spieler 2 at: " + position+"-"+pos);
+                    nextplayerneedto(pos, position);
                     // Spieler 2
                     innerCell.setText("<html><span style=\"color: blue; font-size: 20px;\">o</span></html>");
                     //System.out.println("Spieler 2 ist am Zug");
                     currentPlayerL.setText("Am Zug: Spieler 1");
                     whichplayerturn = false;
+
                 }
 
                 // Label nach dem Setzen des Texts nicht mehr klickbar machen
@@ -150,4 +161,49 @@ public class UI {
 
         return innerCell;
     }
+
+    private void nextplayerneedto(Pos pos, Pos position) {
+        lastmove = pos;
+        switch(lastmove) {
+            case position.UPPERLEFT:
+                nextmove = Pos.UPPERLEFT;
+                System.out.println(nextmove);// Wenn X in UPPERLEFT im kleinen Spielfeld spielt, darf Y in UPPERLEFT im großen Spielfeld spielen
+                break;
+            case position.UPPERMID:
+                System.out.println(nextmove = Pos.UPPERMID);
+                nextmove = Pos.UPPERMID;// Wenn X in UPPERMID im kleinen Spielfeld spielt, darf Y in UPPERMID im großen Spielfeld spielen
+                break;
+            case position.UPPERRIGHT:
+                nextmove = Pos.UPPERRIGHT;
+                System.out.println(nextmove);// Wenn X in UPPERRIGHT im kleinen Spielfeld spielt, darf Y in UPPERRIGHT im großen Spielfeld spielen
+                break;
+            case position.CENTERLEFT:
+                nextmove = Pos.UPPERRIGHT;
+                System.out.println(nextmove);// Wenn X in CENTERLEFT im kleinen Spielfeld spielt, darf Y in CENTERLEFT im großen Spielfeld spielen
+                break;
+            case position.CENTERMID:
+                nextmove = Pos.CENTERMID;
+                System.out.println(nextmove);// Wenn X in CENTERMID im kleinen Spielfeld spielt, darf Y in CENTERMID im großen Spielfeld spielen
+                break;
+            case position.CENTERRIGHT:
+                System.out.println(nextmove = Pos.CENTERRIGHT);
+                nextmove = Pos.CENTERRIGHT;// Wenn X in CENTERRIGHT im kleinen Spielfeld spielt, darf Y in CENTERRIGHT im großen Spielfeld spielen
+                break;
+            case position.LOWERLEFT:
+                nextmove = Pos.LOWERLEFT;
+                System.out.println(nextmove);// Wenn X in LOWERLEFT im kleinen Spielfeld spielt, darf Y in LOWERLEFT im großen Spielfeld spielen
+                break;
+            case position.LOWERMID:
+                nextmove = Pos.LOWERMID;
+                System.out.println(nextmove);// Wenn X in LOWERMID im kleinen Spielfeld spielt, darf Y in LOWERMID im großen Spielfeld spielen
+                break;
+            case position.LOWERRIGHT:
+                nextmove = Pos.LOWERRIGHT;
+                System.out.println(nextmove);// Wenn X in LOWERRIGHT im kleinen Spielfeld spielt, darf Y in LOWERRIGHT im großen Spielfeld spielen
+                break;
+        }
+    }
+
+
+
 }
