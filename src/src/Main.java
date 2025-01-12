77568877f569a119143f50355dd1defa3d0fe65a
Tom <tom@pixelprinzen.de>
2025-01-12 18:41:10 +0100

    added comment on main

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,5 @@
 
-
+//noah beispiel git zeigen
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
